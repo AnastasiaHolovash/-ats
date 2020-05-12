b017be7649d208dd10b22fbfaefff4c2f770d4e0
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2020-05-12 22:03:47 +0300

    Initial Commit

---
+++ Сats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Сats
+//
+//  Created by Головаш Анастасия on 12.05.2020.
+//  Copyright © 2020 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
